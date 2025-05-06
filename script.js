1f32bc36fe27833df4d242e877d321e43df68e31
kartik <kartikkumar0495@gmail.com>
2025-05-06 22:57:12 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Thank you for contacting Elegant Stitch! We will get back to you soon.');
+  });
+  
